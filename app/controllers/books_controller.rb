d04fff1555b87673d39a4837b3d820f4218edd20
Ayachicken <digimonmon.a@gmail.com>
2020-04-28 18:50:49 +0900

    残り、画像にリンクを実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only:[:edit]
+
   def index
   	@books = Book.all
     @book = Book.new
@@ -7,32 +9,53 @@ class BooksController < ApplicationController
 
   def show
   	@book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def create
   	@book = Book.new(book_params)
     @book.user_id = current_user.id
+    @user = @book.user
   	if @book.save
   		flash[:notice] = 'Book was successfully created.'
   		redirect_to book_path(@book.id)
-  	else
-  		render '/users/show'
+    else
+      @books = Book.all
+      render :index
     end
   end
 
   def edit
-  	@book = Bood.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def update
-  	
+  	@book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = 'Book was successfully updated.'
+       redirect_to book_path(@book.id)
+    else
+      render '/books/edit'
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+       flash[:notice] = 'Book was successfully destroyed.'
+       redirect_to user_path(current_user.id)
+    end
   end
 
   private
   def book_params
   	params.require(:book).permit(:title, :body)
   end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+       redirect_to books_path
+    end
+   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+#以下追記
+Refile.secret_key = 'bbc352c047f6b0c40fca390c3e5327a7d4df9fa9035e688fa3e41def8df8804093cdd398e2f65da877dcd026c93fcd8bed8d3da97ce0860f67700093e0b02ba7'
